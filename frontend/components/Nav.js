d162400f922e9b660192a7dccc8bd008eb6967ab
kanastasoff <kireprogramista@gmail.com>
2020-10-19 21:01:19 +0300

    Custom app.js layout

---
+++ frontend/components/Nav.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+const Nav = () => (
+    <div>
+   <Link href="/sell">
+            <a>Sell!</a>
+        </Link>
+        <Link href="/">
+            HOME
+        </Link>
+    </div>
+)
+
+export default Nav;
