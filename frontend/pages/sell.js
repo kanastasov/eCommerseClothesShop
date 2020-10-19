3bd376db9e4d264095dd6b079aaaa6d2546be1f5
kanastasoff <kireprogramista@gmail.com>
2020-10-19 18:40:53 +0300

    Getting started with Home and Sell pages

---
+++ frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <p>Sell!</p>
+        <Link href="/">
+            HOME
+        </Link>
+    </div>
+)
+
+export default Sell;
