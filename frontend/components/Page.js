c0b79229044ee6044c892a2454d8f502d99893a4
kanastasoff <kireprogramista@gmail.com>
2020-10-20 18:32:45 +0300

    Styled components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+    background:red;
+    font-size: ${props => props.huge? '100px': '30px'};
+`;
+
+const BigPoop = styled.span
+`
+    font-size:200px;
+`;
+
 class Page extends Component {
     render() {
         return (
             <div>
                 <Meta/>
              <Header />
+             <MyButton >
+                 Click Me
+                <BigPoop>poop</BigPoop>
+             </MyButton>
+
+             <MyButton huge>
+                 Click Me
+                <BigPoop>poop</BigPoop>
+             </MyButton>
                 {this.props.children}
             </div>
         );
